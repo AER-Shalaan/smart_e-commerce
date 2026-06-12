c7220886d0a6f99891519fe7e81a1248f8007ee8
AbdelRahman Shalan <abdelrahman.shalaan03@gmail.com>
2026-06-12 16:39:00 +0300

    feat: implement LoginButton widget for user authentication

--- lib/layouts/authentication/login/widgets/login_form.dart
+++ lib/layouts/authentication/login/widgets/login_form.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_ecommerce/layouts/authentication/login/widgets/forget_password_text.dart';
-import 'package:smart_ecommerce/layouts/authentication/login/widgets/logIn_button.dart';
+import 'package:smart_ecommerce/layouts/authentication/login/widgets/log_in_button.dart';
 import 'package:smart_ecommerce/layouts/authentication/login/widgets/social_login_buttons.dart';
 import 'login_text_fields.dart';
 
